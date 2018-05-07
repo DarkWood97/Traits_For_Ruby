ec70341edb19f38d6f3cf30d6ae2b191b043242f
lromano97 <lromano@est.frba.utn.edu.ar>
2018-05-06 23:44:37 -0300

    Agrego distintas estrategias

--- src/class.rb
+++ src/class.rb
@@ -9,10 +9,20 @@ class Class
     include Strategy
     if const_get(:Strategy) == Strategy
       method = instance_method(:default_strategy)
+      new_module = method.bind(self).call a_module
+    elsif const_get(:Strategy).is_a? Array
+      method = instance_method(const_get(:Strategy)[0])
+      new_module = method.bind(self).call const_get(:Strategy)[1], a_module
+    elsif const_get(:Strategy).is_a? Proc
+      new_module = const_get(:Strategy).call a_module
     else
       method = instance_method(const_get(:Strategy))
+      new_module = method.bind(self).call a_module
     end
-    new_module = method.bind(self).call a_module
+
     include new_module
   end
 end
+
+estragia = {:default_strategy=> [metodo1, metodo2], :execute_all=>[metodo3]}
+
